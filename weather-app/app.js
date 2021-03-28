982c42c747dac688e0fdc86343a915f38f88f8bc
Aashir Haque <aashirulhaque@gmail.com>
2021-03-28 20:29:24 +0500

    take address arg form command

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,23 +1,29 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode.forward('gulshan e iqbal, block 2, karachi', (error, geoData) => {
+const address = process.argv[2]
 
-    if(error){
-        return console.log(error)
-    }
-
-    const { placeName, lat, long } = geoData
-    forecast.current(lat, long, (error, foreCastData) => {
+if(address){
+    geocode.forward(address, (error, geoData) => {
 
         if(error){
             return console.log(error)
         }
-
-        const { summery, temperature, feelsLike } = foreCastData
-
-        console.log(`LOCATION: ${placeName}`)
-        console.log(`SUMMERY: ${summery}`)
-        console.log(`DESCRIPTION: It is currently ${temperature} degree and feels like ${feelsLike} degree`)
+    
+        const { placeName, lat, long } = geoData
+        forecast.current(lat, long, (error, foreCastData) => {
+    
+            if(error){
+                return console.log(error)
+            }
+    
+            const { summery, temperature, feelsLike } = foreCastData
+    
+            console.log(`LOCATION: ${placeName}`)
+            console.log(`SUMMERY: ${summery}`)
+            console.log(`DESCRIPTION: It is currently ${temperature} degree and feels like ${feelsLike} degree`)
+        })
     })
-})
+}else{
+    console.log('Please provide an address.')
+}
